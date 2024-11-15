83eb06d925e53709c8e56bd215b1fa4605105e43
NSDeveloper18 <sbotirov600@gmail.com>
2024-11-15 19:08:08 +0500

    Initial Commit

---
+++ CardiAlertApp/CardiAlertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardiAlertAppApp.swift
+//  CardiAlertApp
+//
+//  Created by Shakhzod Botirov on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CardiAlertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardiAlertApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardiAlertApp
+//
+//  Created by Shakhzod Botirov on 15/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
